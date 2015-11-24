9ea695d6e78692b7935a5b4a9eb41848676d0a06
AnhNguyen <anhn@asiantech.vn>
2015-11-24 14:05:32 +0700

    change version express

--- app.js
+++ app.js
@@ -1,36 +1,58 @@
+var express = require('express');
+var path = require('path');
+// var favicon = require('serve-favicon');
+// var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
+// var session = require('express-session');
 
-/**
- * Module dependencies.
- */
+var routes = require('./routes/index');
 
-var express = require('express')
-  , routes = require('./routes');
-
-var app = module.exports = express.createServer();
+var app = express();
+var server = require('http').createServer(app);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.favicon(__dirname + '/public/images/favicon.ico')); 
 app.set('port', process.env.PORT || 9000);
-// Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.code = 404;
+  res.send(err);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
+server.listen(app.get("port"), function () {
+  console.log('Server listening at port %d', app.get("port"));
 });
 
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(app.get("port"), function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+module.exports = app;
